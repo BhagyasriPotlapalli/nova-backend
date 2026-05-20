b573aa0744159571b30bdc5d0e7b2274cff97383
BhagyasriPotlapalli <bhagyasripotlapalli@gmail.com>
2026-05-20 17:47:13 +0530

    chore: changes in product data get call

--- config/config.cjs
+++ config/config.cjs
@@ -1,40 +1,4 @@
-// {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_development",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
 
-
-
-// require("dotenv").config();
-
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//   },
-// };
 
 
 require("dotenv").config();
@@ -48,4 +12,13 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: "mysql",
   },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  },
+
 };
